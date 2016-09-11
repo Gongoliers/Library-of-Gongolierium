0aab81d6f62d55bc492d37b47184be5943b099b5
Kyle Corry <kylecorry31@gmail.com>
2016-09-11 18:02:16 -0400

    Add Flywheel abstract class

---
+++ src/com/thegongoliers/output/Flywheel.java
@@ -0,0 +1,13 @@
+package com.thegongoliers.output;
+
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public abstract class Flywheel extends Subsystem {
+
+	public abstract void spinUp(double speed);
+
+	public abstract void stop();
+
+	public abstract void spinToSpeed(double speed);
+
+}
