378a78fe05bab296352684bfa5c992692825e2aa
Kyle Corry <kylecorry31@gmail.com>
2016-11-01 21:27:11 -0400

    Add JavaDoc comments to AccelerometerInterface

--- src/com/thegongoliers/input/AccelerometerInterface.java
+++ src/com/thegongoliers/input/AccelerometerInterface.java
@@ -3,14 +3,38 @@ package com.thegongoliers.input;
 import com.thegongoliers.util.LinearAlgebra.Vector;
 
 public interface AccelerometerInterface {
+
+	/**
+	 * Gets the tilt of the robot (angle to ground).
+	 * 
+	 * @return The tilt of the robot.
+	 */
 	public double getTilt();
 
+	/**
+	 * Gets the Y-Axis acceleration of the robot.
+	 * 
+	 * @return The Y-Axis acceleration.
+	 */
 	public double getY();
 
+	/**
+	 * Gets the X-Axis acceleration of the robot.
+	 * 
+	 * @return The X-Axis acceleration.
+	 */
 	public double getX();
 
+	/**
+	 * Gets the Z-Axis acceleration of the robot.
+	 * 
+	 * @return The Z-Axis acceleration of the robot.
+	 */
 	public double getZ();
 
+	/**
+	 * Display the values of the accelerometer to the SmartDashboard.
+	 */
 	public void display();
 
 	/**
