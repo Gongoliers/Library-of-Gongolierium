9b4bbca6fe94d681eb9f1e783ce1862fbd60a449
Kyle Corry <kylecorry31@gmail.com>
2016-11-01 21:21:17 -0400

    PDPCurrentSensor implements PIDSource

--- src/com/thegongoliers/input/PDPCurrentSensor.java
+++ src/com/thegongoliers/input/PDPCurrentSensor.java
@@ -1,22 +1,38 @@
 package com.thegongoliers.input;
 
+import edu.wpi.first.wpilibj.PIDSource;
+import edu.wpi.first.wpilibj.PIDSourceType;
 import edu.wpi.first.wpilibj.PowerDistributionPanel;
 
 /**
  * Created by Kyle on 7/25/2016.
  */
-public class PDPCurrentSensor {
+public class PDPCurrentSensor implements PIDSource {
 
-    private int port;
-    private PowerDistributionPanel pdp;
+	private int port;
+	private PowerDistributionPanel pdp;
 
-    public PDPCurrentSensor(PowerDistributionPanel pdp, int port) {
-        this.pdp = pdp;
-        this.port = port;
-    }
+	public PDPCurrentSensor(PowerDistributionPanel pdp, int port) {
+		this.pdp = pdp;
+		this.port = port;
+	}
 
-    public double getCurrent() {
-        return pdp.getCurrent(port);
-    }
-    
+	public double getCurrent() {
+		return pdp.getCurrent(port);
+	}
+
+	@Override
+	public void setPIDSourceType(PIDSourceType pidSource) {
+	}
+
+	@Override
+	public PIDSourceType getPIDSourceType() {
+		return PIDSourceType.kDisplacement;
+	}
+
+	@Override
+	public double pidGet() {
+		return getCurrent();
+	}
+
 }
