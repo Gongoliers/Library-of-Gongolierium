6390a2bba0efa497f06ea369c686a56ea7c53ee3
Kyle Corry <kylecorry31@gmail.com>
2016-12-11 16:18:57 -0500

    Add GyroRobotDrive

--- src/com/thegongoliers/output/RobotDrivingUtils.java
+++ src/com/thegongoliers/output/RobotDrivingUtils.java
@@ -4,50 +4,6 @@ import edu.wpi.first.wpilibj.RobotDrive;
 
 public class RobotDrivingUtils {
 
-	private static double initialGyroAngle = 0.0;
-
-	/**
-	 * Set the initial gyro angle of the robot, used mainly for manual
-	 * correction of the gyroStabilizedOperation method.
-	 * 
-	 * @param angle
-	 *            The new initial angle of the robot
-	 */
-	public static void setInitialGyroAngle(double angle) {
-		initialGyroAngle = angle;
-	}
-
-	/**
-	 * Allows the robot to correct drift using the gyro during normal, operator
-	 * controlled driving.
-	 * 
-	 * @param robotDrive
-	 *            The main RobotDrive class for your robot's drivetrain (uses
-	 *            arcadeDrive)
-	 * @param y
-	 *            The speed from -1 to 1 that the robot will go in the y
-	 *            direction
-	 * @param rotation
-	 *            The speed from -1 to 1 that the robot will turn
-	 * @param gyroAngle
-	 *            The current angle of the robot
-	 * @param rotationThreshold
-	 *            The minimum amount of joystick rotation input needed to
-	 *            manually turn the robot
-	 * @param correctionFactor
-	 *            The magnitude of the correction that the robot will take to
-	 *            drive straight (0.01 worked well in the past)
-	 */
-	public static void gyroStabilizedOperation(RobotDrive robotDrive, double y, double rotation, double gyroAngle,
-			double rotationThreshold, double correctionFactor) {
-		if (Math.abs(rotation) >= rotationThreshold) {
-			robotDrive.arcadeDrive(y, rotation);
-			setInitialGyroAngle(gyroAngle);
-		} else {
-			robotDrive.arcadeDrive(y, -correctionFactor * (gyroAngle - initialGyroAngle));
-		}
-	}
-
 	public static void forward(RobotDrive robotDrive, double speed) {
 		robotDrive.drive(-speed, 0);
 	}
