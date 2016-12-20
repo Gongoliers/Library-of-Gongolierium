0cc289545363c54a1349aa06b756bad30a6e1e7e
Kyle Corry <kylecorry31@gmail.com>
2016-12-20 10:55:20 -0500

    Remove Gyro from RotateToAngle

--- src/com/thegongoliers/commands/RotateToAngle.java
+++ src/com/thegongoliers/commands/RotateToAngle.java
@@ -2,7 +2,6 @@ package com.thegongoliers.commands;
 
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.PIDSubsystem;
-import edu.wpi.first.wpilibj.interfaces.Gyro;
 
 /**
  * A command to rotate the robot to face a specific angle.
@@ -18,15 +17,12 @@ public class RotateToAngle extends Command {
 	 * 
 	 * @param robotDriveTrain
 	 *            A PIDSubsystem which can be controlled by the gyroscope.
-	 *            Preferably a drivetrain.
-	 * @param gyroscope
-	 *            The gyroscope on the robot.
 	 * @param targetAngle
 	 *            The amount of degrees from the current position to rotate.
 	 */
-	public RotateToAngle(PIDSubsystem robotDriveTrain, Gyro gyroscope, double targetAngle) {
+	public RotateToAngle(PIDSubsystem robotDriveTrain, double targetAngle) {
 		requires(robotDriveTrain);
-		this.targetAngle = gyroscope.getAngle() + targetAngle;
+		this.targetAngle = targetAngle;
 		this.robotDriveTrain = robotDriveTrain;
 	}
 
@@ -37,7 +33,7 @@ public class RotateToAngle extends Command {
 
 	@Override
 	protected void execute() {
-		robotDriveTrain.setSetpoint(targetAngle);
+		robotDriveTrain.setSetpointRelative(targetAngle);
 	}
 
 	@Override
