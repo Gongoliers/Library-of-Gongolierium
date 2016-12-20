9a0da97ba129c35e76b21e543f44784856c04c32
Kyle Corry <kylecorry31@gmail.com>
2016-12-20 11:28:15 -0500

    Reformat CameraInterface

--- src/com/thegongoliers/input/camera/CameraInterface.java
+++ src/com/thegongoliers/input/camera/CameraInterface.java
@@ -6,7 +6,7 @@ import com.thegongoliers.output.Displayable;
 
 import edu.wpi.cscore.VideoSource;
 
-public interface CameraInterface extends Displayable{
+public interface CameraInterface extends Displayable {
 	/**
 	 * Get the view angle of the camera in degrees.
 	 * 
