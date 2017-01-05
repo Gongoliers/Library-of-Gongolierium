160232bdf12456e462abdf0963876bcfd7695043
Kyle Corry <kylecorry31@gmail.com>
2017-01-05 11:24:09 -0500

    Modify JoystickTransformer to remove Point; remove JoystickInput

--- src/com/thegongoliers/input/JoystickTransformer.java
+++ src/com/thegongoliers/input/JoystickTransformer.java
@@ -1,24 +1,8 @@
 package com.thegongoliers.input;
 
-import com.thegongoliers.geometry.Point;
 import com.thegongoliers.math.MathExt;
 
 public class JoystickTransformer {
-
-	/**
-	 * Changes the sensitivity of the joystick values. Call after applying other
-	 * adjustments to the joystick values.
-	 * 
-	 * @param input
-	 *            The values of the joystick.
-	 * @param sensitivity
-	 *            The sensitivity (greater than 0, where 0 is no movement)
-	 * @return The adjusted controller input with the sensitivity applied.
-	 */
-	public static Point sensitivity(Point input, double sensitivity) {
-		return new Point(sensitivity(input.x, sensitivity), sensitivity(input.y, sensitivity),
-				sensitivity(input.z, sensitivity));
-	}
 
 	/**
 	 * Changes the sensitivity of the joystick value. Call after applying other
@@ -44,26 +28,12 @@ public class JoystickTransformer {
 	 *            The smoothing power.
 	 * @return The smoothed joystick value.
 	 */
-	public static Point power(Point input, double pow) {
-		return new Point(power(input.x, pow), power(input.y, pow), power(input.z, pow));
-	}
-
-	/**
-	 * Smoothes the value of the joystick by a power. Should be applied after a
-	 * deadzone transformation.
-	 * 
-	 * @param input
-	 *            The value of the joystick.
-	 * @param pow
-	 *            The smoothing power.
-	 * @return The smoothed joystick value.
-	 */
 	public static double power(double input, double pow) {
 		return MathExt.sign(input) * Math.pow(Math.abs(input), pow);
 	}
 
 	/**
-	 * Blocks movement within a threshold on each axis..
+	 * Blocks movement within a threshold on an axis.
 	 * 
 	 * @param input
 	 *            The value of the joystick.
@@ -71,21 +41,7 @@ public class JoystickTransformer {
 	 *            The deadzone threshold.
 	 * @return The adjusted joystick value with the deadzone.
 	 */
-	public static Point axialDeadzone(Point input, double thresh) {
-		return new Point(axialDeadzone(input.x, thresh), axialDeadzone(input.y, thresh),
-				axialDeadzone(input.z, thresh));
-	}
-
-	/**
-	 * Blocks movement within a threshold on each axis..
-	 * 
-	 * @param input
-	 *            The value of the joystick.
-	 * @param thresh
-	 *            The deadzone threshold.
-	 * @return The adjusted joystick value with the deadzone.
-	 */
-	public static double axialDeadzone(double input, double thresh) {
+	public static double deadzone(double input, double thresh) {
 		if (Math.abs(input) < thresh)
 			return 0.0;
 		return input;
@@ -94,40 +50,40 @@ public class JoystickTransformer {
 	/**
 	 * Blocks movement with a radius of (0, 0)
 	 * 
-	 * @param input
-	 *            The value of the joystick.
+	 * @param x
+	 *            The x value of the joystick.
+	 * @param y
+	 *            The y value of the joystick.
 	 * @param thresh
 	 *            The deadzone threshold.
-	 * @return The adjusted joystick value with the deadzone.
+	 * @return The adjusted joystick value with the deadzone where x is at
+	 *         position 0 and y is at position 1.
 	 */
-	public static Point radialDeadzone(Point input, double thresh) {
-		if (magnitude(input) < thresh)
-			return Point.origin;
-		return input;
+	public static double[] radialDeadzone(double x, double y, double thresh) {
+		if (Math.sqrt(x * x + y * y) < thresh)
+			return new double[2];
+		return new double[] { x, y };
 	}
 
 	/**
 	 * Blocks movement with a radius of (0, 0) and scales the resulting good
 	 * zones from 0 to +/- 1
 	 * 
-	 * @param input
-	 *            The value of the joystick.
+	 * @param x
+	 *            The x value of the joystick.
+	 * @param y
+	 *            The y value of the joystick.
 	 * @param thresh
 	 *            The deadzone threshold.
-	 * @return The adjusted joystick value with the deadzone.
+	 * @return The adjusted joystick value with the scaled deadzone where x is
+	 *         at position 0 and y is at position 1.
 	 */
-	public static Point scaledRadialDeadzone(Point input, double thresh) {
-		if (magnitude(input) < thresh)
-			return Point.origin;
-		return normalized(input).multiply((magnitude(input) - thresh) / (1 - thresh));
-	}
-
-	private static Point normalized(Point p) {
-		double mag = magnitude(p);
-		return new Point(p.x / mag, p.y / mag, p.z / mag);
-	}
-
-	private static double magnitude(Point p) {
-		return Math.sqrt(p.x * p.x + p.y * p.y + p.z * p.z);
+	public static double[] scaledRadialDeadzone(double x, double y, double thresh) {
+		double mag = Math.sqrt(x * x + y * y);
+		if (mag < thresh)
+			return new double[2];
+		double newX = (x / mag) * ((mag - thresh) / (1 - thresh));
+		double newY = (y / mag) * ((mag - thresh) / (1 - thresh));
+		return new double[] { newX, newY };
 	}
 }
