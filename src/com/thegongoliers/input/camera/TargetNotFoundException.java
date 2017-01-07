85cece5578d856314da94a678dfa860330cad9ad
Kyle Corry <kylecorry31@gmail.com>
2017-01-07 16:25:02 -0500

    Add JavaDoc comments to TargetNotFoundException

--- src/com/thegongoliers/input/camera/TargetNotFoundException.java
+++ src/com/thegongoliers/input/camera/TargetNotFoundException.java
@@ -1,9 +1,16 @@
 package com.thegongoliers.input.camera;
 
+/**
+ * Thrown if the camera was unable to locate the target.
+ *
+ */
 public class TargetNotFoundException extends Exception {
 
 	private static final long serialVersionUID = -8838872167969870003L;
 
+	/**
+	 * Constructs a TargetNotFoundException.
+	 */
 	public TargetNotFoundException() {
 		super("A target was not found");
 	}
