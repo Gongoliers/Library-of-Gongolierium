daf27dbcc33b78093f850076d720e8e0bd6f39f0
Kyle Corry <kylecorry31@gmail.com>
2017-01-07 16:27:56 -0500

    Add JavaDoc comments to TargetReportInterface

--- src/com/thegongoliers/input/camera/TargetReportInterface.java
+++ src/com/thegongoliers/input/camera/TargetReportInterface.java
@@ -4,12 +4,34 @@ import com.thegongoliers.geometry.Point;
 
 interface TargetReportInterface {
 
+	/**
+	 * The confidence from 0 to 100 that the target located matched the given
+	 * target specifications.
+	 * 
+	 * @return The confidence from 0 to 100 inclusive.
+	 */
 	int confidence();
 
+	/**
+	 * The angle to the target in degrees.
+	 * 
+	 * @return The angle in degrees.
+	 */
 	double angle();
 
+	/**
+	 * The distance to the target.
+	 * 
+	 * @return The distance.
+	 */
 	double distance();
 
+	/**
+	 * The location of the target in the image where each axis is from [-1, 1]
+	 * and 0 is the center.
+	 * 
+	 * @return The location of the target in aiming coordinates.
+	 */
 	Point aimingCoordinates();
 
 }
