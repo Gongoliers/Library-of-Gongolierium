f81afe74ad0192ae0f816b48fb8a9909b7dedcc2
Kyle Corry <kylecorry31@gmail.com>
2017-01-07 21:26:44 -0500

    JavaDocs for Hardware

--- src/com/thegongoliers/hardware/Hardware.java
+++ src/com/thegongoliers/hardware/Hardware.java
@@ -8,10 +8,26 @@ import edu.wpi.first.wpilibj.interfaces.Gyro;
 
 public class Hardware {
 
+	/**
+	 * Join motors together to control synchronously with a single
+	 * SpeedController.
+	 * 
+	 * @param controllers
+	 *            The controllers to join together.
+	 * @return A SpeedController which combines all the SpeedControllers
+	 *         together.
+	 */
 	public static SpeedController joinMotors(SpeedController... controllers) {
 		return new JoinedSpeedController(controllers);
 	}
 
+	/**
+	 * Invert the direction of the Gyro.
+	 * 
+	 * @param gyro
+	 *            The Gyro to invert.
+	 * @return The inverted Gyro.
+	 */
 	public static Gyro invertGyro(Gyro gyro) {
 		return new Gyro() {
 
