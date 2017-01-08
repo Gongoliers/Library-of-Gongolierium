8632b81f75833b182a1a273f0b5bb68e68bc627e
Kyle Corry <kylecorry31@gmail.com>
2017-01-07 21:03:50 -0500

    Add JavaDoc comments to the LEDColor

--- src/com/thegongoliers/input/camera/LEDColor.java
+++ src/com/thegongoliers/input/camera/LEDColor.java
@@ -2,14 +2,37 @@ package com.thegongoliers.input.camera;
 
 import org.opencv.core.Range;
 
+/**
+ * Represents the color of an LED in HSV
+ *
+ */
 public interface LEDColor {
+
+	/**
+	 * Gets the hue of the LED.
+	 * 
+	 * @return The LED's hue.
+	 */
 	Range getHue();
 
+	/**
+	 * Gets the saturation of the LED.
+	 * 
+	 * @return The LED's saturation.
+	 */
 	Range getSaturation();
 
+	/**
+	 * Gets the value of the LED.
+	 * 
+	 * @return The LED's value.
+	 */
 	Range getValue();
 
-	public static LEDColor Green = new LEDColor(){
+	/**
+	 * A green superbright LED ring which produces light at 518 nm.
+	 */
+	public static LEDColor Green = new LEDColor() {
 		@Override
 		public Range getHue() {
 			return new Range(75, 125);
