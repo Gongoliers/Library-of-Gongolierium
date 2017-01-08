890e77fbda1498340a682d6fcd923b5d95db81cf
Kyle Corry <kylecorry31@gmail.com>
2017-01-08 17:58:06 -0500

    Document Statistics

--- src/com/thegongoliers/math/Statistics.java
+++ src/com/thegongoliers/math/Statistics.java
@@ -3,10 +3,25 @@ package com.thegongoliers.math;
 import java.util.Arrays;
 
 public class Statistics {
+
+	/**
+	 * Calculates the statistical mean of the given data.
+	 * 
+	 * @param data
+	 *            The data.
+	 * @return The mean of the data.
+	 */
 	public static double mean(double[] data) {
 		return MathExt.sum(data) / data.length;
 	}
 
+	/**
+	 * Calculates the median of the given data.
+	 * 
+	 * @param data
+	 *            The data.
+	 * @return The median of the data.
+	 */
 	public static double median(double[] data) {
 		double[] sorted = Arrays.copyOf(data, data.length);
 		Arrays.sort(sorted);
@@ -17,6 +32,13 @@ public class Statistics {
 		}
 	}
 
+	/**
+	 * Calculates the variance of the given data.
+	 * 
+	 * @param data
+	 *            The data.
+	 * @return The variance of the data.
+	 */
 	public static double variance(double[] data) {
 		double sum = 0;
 		double mean = mean(data);
@@ -29,10 +51,24 @@ public class Statistics {
 		return sum / n;
 	}
 
+	/**
+	 * Calculates the standard deviation of the given data.
+	 * 
+	 * @param data
+	 *            The data.
+	 * @return The standard deviation of the data.
+	 */
 	public static double standardDeviation(double[] data) {
 		return Math.sqrt(variance(data));
 	}
 
+	/**
+	 * Calculates the mean absolute deviation of the given data.
+	 * 
+	 * @param data
+	 *            The data.
+	 * @return The mean absolute deviation of the data.
+	 */
 	public static double meanAbsoluteDeviation(double[] data) {
 		double sum = 0;
 		double mean = mean(data);
@@ -45,6 +81,17 @@ public class Statistics {
 		return sum / n;
 	}
 
+	/**
+	 * Calculates the moving average of the given data point.
+	 * 
+	 * @param prevData
+	 *            The previous data points (not moving average).
+	 * @param currentValue
+	 *            The current data point.
+	 * @param size
+	 *            The size of the moving average window.
+	 * @return The moving average of the data point.
+	 */
 	public static double movingAverage(double[] prevData, double currentValue, int size) {
 		size = Math.min(prevData.length, size - 1);
 		double sum = 0;
@@ -55,6 +102,15 @@ public class Statistics {
 		return sum / (size + 1.0);
 	}
 
+	/**
+	 * Calculates the moving average over the given data.
+	 * 
+	 * @param values
+	 *            The data.
+	 * @param size
+	 *            The size of the moving average window.
+	 * @return The moving average of the data.
+	 */
 	public static double[] movingAverage(double[] values, int size) {
 		double[] ma = new double[values.length];
 		for (int i = 0; i < values.length; i++) {
