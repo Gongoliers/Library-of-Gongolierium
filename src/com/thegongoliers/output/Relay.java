966a248d85fce9ef825dd1bcf4d91737a4b37017
Kyle Corry <kylecorry31@gmail.com>
2017-01-08 17:53:59 -0500

    Document Relay

--- src/com/thegongoliers/output/Relay.java
+++ src/com/thegongoliers/output/Relay.java
@@ -1,11 +1,28 @@
 package com.thegongoliers.output;
 
 public interface Relay {
+
+	/**
+	 * Turn the relay on.
+	 */
 	void on();
 
+	/**
+	 * Turn the relay off.
+	 */
 	void off();
 
+	/**
+	 * Determines if the relay is on.
+	 * 
+	 * @return True if the relay is on.
+	 */
 	boolean isOn();
 
+	/**
+	 * Determines if the relay is off.
+	 * 
+	 * @return True if the relay is off.
+	 */
 	boolean isOff();
 }
