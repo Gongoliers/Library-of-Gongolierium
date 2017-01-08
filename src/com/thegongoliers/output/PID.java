e774669bc9b5112d8e5a059594bcc90355fba618
Kyle Corry <kylecorry31@gmail.com>
2017-01-08 17:52:30 -0500

    Document PID

--- src/com/thegongoliers/output/PID.java
+++ src/com/thegongoliers/output/PID.java
@@ -12,6 +12,21 @@ public class PID {
 	boolean continuous = false;
 	public double minInput = 0, maxInput = 1;
 
+	/**
+	 * Creates a simple PID calculator.
+	 * 
+	 * @param kp
+	 *            The proportional constant.
+	 * @param ki
+	 *            The integral constant.
+	 * @param kd
+	 *            The differential constant.
+	 * @param threshold
+	 *            The threshold at which the sensor is considered on target.
+	 * @param continuous
+	 *            True if the device is continuous (for example if rotating 360
+	 *            degrees from 0 you end up at 0 it is continuous)
+	 */
 	public PID(double kp, double ki, double kd, double threshold, boolean continuous) {
 		this.kp = kp;
 		this.ki = ki;
@@ -20,6 +35,18 @@ public class PID {
 		this.continuous = continuous;
 	}
 
+	/**
+	 * Creates a simple PID calculator.
+	 * 
+	 * @param kp
+	 *            The proportional constant.
+	 * @param ki
+	 *            The integral constant.
+	 * @param kd
+	 *            The differential constant.
+	 * @param threshold
+	 *            The threshold at which the sensor is considered on target.
+	 */
 	public PID(double kp, double ki, double kd, double threshold) {
 		this.kp = kp;
 		this.ki = ki;
@@ -40,6 +67,16 @@ public class PID {
 		return error;
 	}
 
+	/**
+	 * Calculates the output of the PID.
+	 * 
+	 * @param currentPosition
+	 *            The current position.
+	 * @param targetPosition
+	 *            The target position.
+	 * @return The output of the PID which calculates how to get to the target
+	 *         position from the current position.
+	 */
 	public double getOutput(double currentPosition, double targetPosition) {
 		double error = getContinuousError(targetPosition - currentPosition);
 		if (first) {
@@ -52,6 +89,16 @@ public class PID {
 		return pidOutput;
 	}
 
+	/**
+	 * Determines if the current position is close enough to the target position
+	 * using the threshold.
+	 * 
+	 * @param currentPosition
+	 *            The current position.
+	 * @param targetPosition
+	 *            The target position.
+	 * @return True if it is at the target position (within the threshold).
+	 */
 	public boolean isAtTargetPosition(double currentPosition, double targetPosition) {
 		return Math.abs(currentPosition - targetPosition) <= threshold;
 	}
