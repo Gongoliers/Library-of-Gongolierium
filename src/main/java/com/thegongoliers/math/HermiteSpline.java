857a75354c9735bf6793c78c55655f67551a27fd
Kyle Corry <kylecorry31@gmail.com>
2017-03-05 08:15:14 -0500

    Add HermiteSpline class
    The HermiteSpline class creates a 3rd order spline which allows for
    smooth transitions in between points in space.

---
+++ src/main/java/com/thegongoliers/math/HermiteSpline.java
@@ -0,0 +1,54 @@
+package com.thegongoliers.math;
+
+/**
+ * A Hermite 3rd order spline.
+ */
+public class HermiteSpline {
+
+    private double p0, p1, v0, v1, totalTime;
+
+    /**
+     * Create a Hermite 3rd order spline from p0 to p1 with the initial velocity of v0 and the final velocity of v1. The total time is 1 second.
+     *
+     * @param p0 The initial position.
+     * @param v0 The initial velocity.
+     * @param p1 The final position.
+     * @param v1 The final velocity.
+     */
+    public HermiteSpline(double p0, double v0, double p1, double v1) {
+        this(p0, v0, p1, v1, 1.0);
+    }
+
+    /**
+     * Create a Hermite 3rd order spline from p0 to p1 with the initial velocity of v0 and the final velocity of v1.
+     *
+     * @param p0        The initial position.
+     * @param v0        The initial velocity.
+     * @param p1        The final position.
+     * @param v1        The final velocity.
+     * @param totalTime The total time of the spline.
+     */
+    public HermiteSpline(double p0, double v0, double p1, double v1, double totalTime) {
+        this.p0 = p0;
+        this.p1 = p1;
+        this.v0 = v0;
+        this.v1 = v1;
+        this.totalTime = totalTime;
+    }
+
+    /**
+     * Calculate the position of the spline at a given time.
+     *
+     * @param time The time in seconds of the spline (between 0 and the totalTime - totalTime defaults to 1)
+     * @return The position of the spline at the given time.
+     */
+    public double calculate(double time) {
+        time /= totalTime;
+        double first = (2 * Math.pow(time, 3) - 3 * Math.pow(time, 2) + 1) * p0;
+        double second = (Math.pow(time, 3) - 2 * Math.pow(time, 2) + time) * v0;
+        double third = (-2 * Math.pow(time, 3) + 3 * Math.pow(time, 2)) * p1;
+        double fourth = (Math.pow(time, 3) - Math.pow(time, 2)) * v1;
+        return first + second + third + fourth;
+    }
+
+}
---
+++ src/test/java/com/thegongoliers/math/HermiteSplineTest.java
@@ -0,0 +1,29 @@
+package com.thegongoliers.math;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Kyle on 3/5/2017.
+ */
+public class HermiteSplineTest {
+    @Test
+    public void calculate() {
+        HermiteSpline hermiteSpline = new HermiteSpline(0, 0, 2, 0);
+        assertEquals(hermiteSpline.calculate(0), 0, 0);
+        assertEquals(hermiteSpline.calculate(1), 2, 0);
+        assertEquals(hermiteSpline.calculate(0.5), 1, 0.0000001);
+        assertEquals(hermiteSpline.calculate(0.75), 1.6875, 0.0001);
+    }
+
+    @Test
+    public void calculateTime() {
+        HermiteSpline hermiteSpline = new HermiteSpline(0, 0, 2, 0, 4);
+        assertEquals(hermiteSpline.calculate(0), 0, 0);
+        assertEquals(hermiteSpline.calculate(2), 1, 0);
+        assertEquals(hermiteSpline.calculate(3), 1.6875, 0.0001);
+        assertEquals(hermiteSpline.calculate(4), 2, 0);
+    }
+
+}
