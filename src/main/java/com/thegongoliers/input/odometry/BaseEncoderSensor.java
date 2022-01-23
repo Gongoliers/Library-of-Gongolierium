2be7a7b45fc53fcbd82428bef94b70fddbcaa42b
Kyle Corry <kylecorry31@gmail.com>
2022-01-23 06:48:17 -0500

    Add base encoder sensor

---
+++ src/main/java/com/thegongoliers/input/odometry/BaseEncoderSensor.java
@@ -0,0 +1,42 @@
+package com.thegongoliers.input.odometry;
+
+import com.thegongoliers.utils.Resettable;
+
+public class BaseEncoderSensor implements EncoderSensor {
+
+    private double mLastDistance = 0.0;
+    private DistanceSensor mDistanceSensor;
+    private VelocitySensor mVelocitySensor;
+    private Resettable mResettable;
+
+    public BaseEncoderSensor(DistanceSensor distance, VelocitySensor velocity){
+        this(distance, velocity, null);
+    }
+
+    public BaseEncoderSensor(DistanceSensor distance, VelocitySensor velocity, Resettable resettable){
+        mDistanceSensor = distance;
+        mVelocitySensor = velocity;
+        mResettable = resettable;
+    }
+
+    @Override
+    public double getDistance() {
+        return mDistanceSensor.getDistance() - mLastDistance;
+    }
+
+    @Override
+    public double getVelocity() {
+        return mVelocitySensor.getVelocity();
+    }
+
+    @Override
+    public void reset() {
+        if (mResettable != null){
+            mResettable.reset();
+            return;
+        }
+
+        mLastDistance = getDistance();
+    }
+    
+}
--- src/main/java/com/thegongoliers/input/odometry/EncoderUtils.java
+++ src/main/java/com/thegongoliers/input/odometry/EncoderUtils.java
@@ -1,5 +1,7 @@
 package com.thegongoliers.input.odometry;
 
+import com.thegongoliers.utils.Resettable;
+
 import edu.wpi.first.wpilibj.Encoder;
 
 public final class EncoderUtils {
@@ -20,6 +22,23 @@ public final class EncoderUtils {
      */
     public static VelocitySensor toVelocitySensor(Encoder encoder) {
         return encoder::getRate;
+    }
+
+    /**
+     * Convert an encoder to an encoder sensor
+     * @param encoder the WPILib encoder
+     * @return the encoder sensor
+     */
+    public static EncoderSensor toEncoderSensor(Encoder encoder){
+        return new WPIEncoderSensor(encoder);
+    }
+
+    public static EncoderSensor toEncoderSensor(DistanceSensor distance, VelocitySensor velocity, Resettable reset){
+        return new BaseEncoderSensor(distance, velocity, reset);
+    }
+
+    public static EncoderSensor toEncoderSensor(DistanceSensor distance, VelocitySensor velocity){
+        return new BaseEncoderSensor(distance, velocity);
     }
 
 }
