a78741ad0e5936e7d1fb70c43e0d0daaf2bf10e2
Kyle Corry <kylecorry31@gmail.com>
2022-03-11 07:11:53 -0500

    Add ramp and scale support to GSpeedController
    Closes #39 and closes #38

--- src/main/java/com/thegongoliers/output/actuators/GSpeedController.java
+++ src/main/java/com/thegongoliers/output/actuators/GSpeedController.java
@@ -4,6 +4,9 @@ import com.kylecorry.pid.PID;
 import com.thegongoliers.input.odometry.DistanceSensor;
 import com.thegongoliers.input.odometry.EncoderSensor;
 import com.thegongoliers.input.odometry.VelocitySensor;
+import com.thegongoliers.input.time.Clock;
+import com.thegongoliers.input.time.RobotClock;
+import com.thegongoliers.math.GMath;
 
 import edu.wpi.first.wpilibj.AnalogPotentiometer;
 import edu.wpi.first.wpilibj.Encoder;
@@ -16,6 +19,21 @@ public class GSpeedController implements MotorController {
     private VelocitySensor mVelocitySensor;
     private PID mDistancePID;
     private PID mVelocityPID;
+    private double mSecondsToFullSpeed;
+    private Clock mClock;
+    private double mLastTime;
+    private double mScale = 1.0;
+
+    /**
+     * A speed controller with added functionality
+     * @param speedController the underlying speed controller
+     * @param encoder the encoder which senses the movement of the motor controlled by the speed controller
+     * @param distancePID the PID for setting a distance (ex. 0.1 when distance is in feet)
+     * @param velocityPID the PID for setting a velocity (ex. 1 / max velocity)
+     */
+    public GSpeedController(MotorController speedController, Encoder encoder, PID distancePID, PID velocityPID, Clock clock){
+        this(speedController, encoder::getDistance, encoder::getRate, distancePID, velocityPID, clock);
+    }
 
     /**
      * A speed controller with added functionality
@@ -25,7 +43,18 @@ public class GSpeedController implements MotorController {
      * @param velocityPID the PID for setting a velocity (ex. 1 / max velocity)
      */
     public GSpeedController(MotorController speedController, Encoder encoder, PID distancePID, PID velocityPID){
-        this(speedController, encoder::getDistance, encoder::getRate, distancePID, velocityPID);
+        this(speedController, encoder::getDistance, encoder::getRate, distancePID, velocityPID, new RobotClock());
+    }
+
+    /**
+     * A speed controller with added functionality
+     * @param speedController the underlying speed controller
+     * @param encoder the encoder which senses the movement of the motor controlled by the speed controller
+     * @param distancePID the PID for setting a distance (ex. 0.1 when distance is in feet)
+     * @param velocityPID the PID for setting a velocity (ex. 1 / max velocity)
+     */
+    public GSpeedController(MotorController speedController, EncoderSensor encoder, PID distancePID, PID velocityPID, Clock clock){
+        this(speedController, encoder::getDistance, encoder::getVelocity, distancePID, velocityPID, clock);
     }
 
     /**
@@ -36,7 +65,18 @@ public class GSpeedController implements MotorController {
      * @param velocityPID the PID for setting a velocity (ex. 1 / max velocity)
      */
     public GSpeedController(MotorController speedController, EncoderSensor encoder, PID distancePID, PID velocityPID){
-        this(speedController, encoder::getDistance, encoder::getVelocity, distancePID, velocityPID);
+        this(speedController, encoder::getDistance, encoder::getVelocity, distancePID, velocityPID, new RobotClock());
+    }
+
+    /**
+     * A speed controller with added functionality
+     * Potentiometers can not use velocity control!
+     * @param speedController the underlying speed controller
+     * @param potentiometer the potentiometer which senses the movement of the motor controlled by the speed controller
+     * @param distancePID the PID for setting a distance
+     */
+    public GSpeedController(MotorController speedController, AnalogPotentiometer potentiometer, PID distancePID, Clock clock){
+        this(speedController, potentiometer::get, () -> 0.0, distancePID, new PID(0, 0, 0), clock);
     }
 
     /**
@@ -47,7 +87,7 @@ public class GSpeedController implements MotorController {
      * @param distancePID the PID for setting a distance
      */
     public GSpeedController(MotorController speedController, AnalogPotentiometer potentiometer, PID distancePID){
-        this(speedController, potentiometer::get, () -> 0.0, distancePID, new PID(0, 0, 0));
+        this(speedController, potentiometer::get, () -> 0.0, distancePID, new PID(0, 0, 0), new RobotClock());
     }
 
     /**
@@ -59,23 +99,55 @@ public class GSpeedController implements MotorController {
      * @param velocityPID the PID for setting a velocity
      */
     public GSpeedController(MotorController speedController, DistanceSensor distanceSensor, VelocitySensor velocitySensor, PID distancePID, PID velocityPID){
+        this(speedController, distanceSensor, velocitySensor, distancePID, velocityPID, new RobotClock());
+    }
+
+    /**
+     * A speed controller with added functionality
+     * @param speedController the underlying speed controller
+     * @param distanceSensor
+     * @param velocitySensor
+     * @param distancePID the PID for setting a distance
+     * @param velocityPID the PID for setting a velocity
+     * @param clock the clock
+     */
+    public GSpeedController(MotorController speedController, DistanceSensor distanceSensor, VelocitySensor velocitySensor, PID distancePID, PID velocityPID, Clock clock){
         mSpeedController = speedController;
         mDistanceSensor = distanceSensor;
         mVelocitySensor = velocitySensor;
         mDistancePID = distancePID;
         mVelocityPID = velocityPID;
+        mClock = clock;
+        mLastTime = mClock.getTime();
     }
 
     @Override
     public void set(double speed) {
-        mSpeedController.set(speed);
+        if (mSecondsToFullSpeed <= 0){
+            mSpeedController.set(speed * mScale);
+        } else {
+            var time = mClock.getTime();
+            var deltaTime = time - mLastTime;
+            mLastTime = time;
+            double maximumRate = getMaxRate(deltaTime);
+            var newSpeed = GMath.rateLimit(maximumRate, speed, get());
+            mSpeedController.set(newSpeed * mScale);
+        }
+    }
+
+    /**
+     * Controls the ramping of the motor. Set to 0 to disable.
+     * @param secondsToReachFullSpeed the ramping time in seconds from 0 to full speed
+     */
+    public void setSecondsToFullSpeed(double secondsToFullSpeed){
+        mSecondsToFullSpeed = Math.abs(secondsToFullSpeed);
     }
 
     /**
      * @param velocity the velocity of the motor in encoder units / second
      */
     public void setVelocity(double velocity){
-        set(mSpeedController.get() + mVelocityPID.calculate(getVelocity(), velocity));
+        set(get() + mVelocityPID.calculate(getVelocity(), velocity));
     }
 
     /**
@@ -101,7 +173,7 @@ public class GSpeedController implements MotorController {
 
     @Override
     public double get() {
-        return mSpeedController.get();
+        return mSpeedController.get() / mScale;
     }
 
     @Override
@@ -114,6 +186,10 @@ public class GSpeedController implements MotorController {
         return mSpeedController.getInverted();
     }
 
+    public void setScale(double scale){
+        mScale = scale;
+    }
+
     @Override
     public void disable() {
         mSpeedController.disable();
@@ -122,6 +198,10 @@ public class GSpeedController implements MotorController {
     @Override
     public void stopMotor() {
         mSpeedController.stopMotor();
+    }
+
+    private double getMaxRate(double deltaTime) {
+        return mSecondsToFullSpeed == 0 ? 1 : deltaTime / mSecondsToFullSpeed;
     }
 
 }
--- src/test/java/com/thegongoliers/output/actuators/GSpeedControllerTest.java
+++ src/test/java/com/thegongoliers/output/actuators/GSpeedControllerTest.java
@@ -6,22 +6,27 @@ import org.mockito.AdditionalMatchers;
 
 import edu.wpi.first.wpilibj.motorcontrol.MotorController;
 
+import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.*;
 
 import com.kylecorry.pid.PID;
 import com.thegongoliers.input.odometry.EncoderSensor;
+import com.thegongoliers.input.time.Clock;
 
 public class GSpeedControllerTest {
 
     private GSpeedController speedController;
     private MotorController mockSpeedController;
     private EncoderSensor mockEncoder;
+    private Clock mockClock;
 
     @Before
     public void setup(){
         mockSpeedController = mock(MotorController.class);
         mockEncoder = mock(EncoderSensor.class);
-        speedController = new GSpeedController(mockSpeedController, mockEncoder, new PID(0.1, 0, 0), new PID(0.2, 0, 0));
+        mockClock = mock(Clock.class);
+        when(mockClock.getTime()).thenReturn(0.0);
+        speedController = new GSpeedController(mockSpeedController, mockEncoder, new PID(0.1, 0, 0), new PID(0.2, 0, 0), mockClock);
     }
 
     @Test
@@ -98,4 +103,50 @@ public class GSpeedControllerTest {
         speedController.getVelocity();
         verify(mockEncoder).getVelocity();
     }
+
+    @Test
+    public void canScale(){
+        speedController.setScale(0.5);
+        speedController.set(0.1);
+        verify(mockSpeedController).set(0.05);
+        when(mockSpeedController.get()).thenReturn(0.05);
+        assertEquals(0.1, speedController.get(), 0.01);
+    }
+
+    @Test
+    public void limitsAccelerationOnUnfixedDT(){
+        speedController.setSecondsToFullSpeed(0.2);
+        when(mockClock.getTime()).thenReturn(0.01);
+        speedController.set(0.1);
+        verify(mockSpeedController).set(AdditionalMatchers.eq(0.05, 0.001));
+        when(mockSpeedController.get()).thenReturn(0.05);
+
+         when(mockClock.getTime()).thenReturn(0.2);
+         speedController.set(0.05);
+         verify(mockSpeedController, times(2)).set(AdditionalMatchers.eq(0.05, 0.001));
+         when(mockSpeedController.get()).thenReturn(0.05);
+
+         when(mockClock.getTime()).thenReturn(0.21);
+         speedController.set(-0.05);
+         verify(mockSpeedController).set(AdditionalMatchers.eq(0.0, 0.001));
+    }
+
+    @Test
+    public void limitsAccelerationOnFixedDT(){
+        speedController.setSecondsToFullSpeed(0.2);
+        when(mockClock.getTime()).thenReturn(0.02);
+        speedController.set(1);
+        verify(mockSpeedController).set(AdditionalMatchers.eq(0.1, 0.001));
+        when(mockSpeedController.get()).thenReturn(0.1);
+
+        when(mockClock.getTime()).thenReturn(0.04);
+        speedController.set(-1);
+        verify(mockSpeedController).set(AdditionalMatchers.eq(0.0, 0.001));
+        when(mockSpeedController.get()).thenReturn(0.0);
+
+        when(mockClock.getTime()).thenReturn(0.06);
+        speedController.set(-0.2);
+        verify(mockSpeedController).set(AdditionalMatchers.eq(-0.1, 0.001));
+    }
+
 }
