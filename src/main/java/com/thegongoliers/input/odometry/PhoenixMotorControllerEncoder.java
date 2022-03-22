c268867eac88bcb655e627e88cfe567eb60134c3
Kyle Corry <kylecorry31@gmail.com>
2022-03-22 08:06:08 -0400

    Add phoenix encoder
    Closes #53

---
+++ src/main/java/com/thegongoliers/input/odometry/PhoenixMotorControllerEncoder.java
@@ -0,0 +1,23 @@
+package com.thegongoliers.input.odometry;
+
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.can.BaseMotorController;
+import com.thegongoliers.input.odometry.BaseEncoderSensor;
+
+public class PhoenixMotorControllerEncoder extends BaseEncoderSensor {
+
+    private BaseMotorController mMotor;
+    private FeedbackDevice mDevice;
+
+    public PhoenixMotorControllerEncoder(BaseMotorController motor, FeedbackDevice device) {
+        super(motor::getSelectedSensorPosition, motor::getSelectedSensorVelocity);
+        mMotor = motor;
+        mDevice = device;
+        reselectFeedbackDevice();
+    }
+
+    public void reselectFeedbackDevice(){
+        mMotor.configSelectedFeedbackSensor(mDevice);
+    }
+
+}
